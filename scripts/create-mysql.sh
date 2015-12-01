4aa6c5055c1a50c7af571e18fd734c9193d19961
Lucas Vasconcelos <lucasvscn@gmail.com>
2015-11-30 22:54:13 -0300

    provision fixes
     - provision don't drop mysql databases anymore
     - timezone set to America/Recife

--- scripts/create-mysql.sh
+++ scripts/create-mysql.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
 DB=$1;
-mysql -uworkstead -psecret -e "DROP DATABASE IF EXISTS $DB";
-mysql -uworkstead -psecret -e "CREATE DATABASE $DB";
+mysql -uworkstead -psecret -e "CREATE DATABASE IF NOT EXISTS $DB";
